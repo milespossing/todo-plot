fdb66fcf1e78177550645413b23988dcc9424267
Miles Possing <7330275+milespossing@users.noreply.github.com>
2023-09-12 08:08:03 -0700

    Updating axis styles

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use chrono::{DateTime, Local, NaiveDateTime};
 use clap::Parser;
-use chrono::{NaiveDateTime, DateTime, Local};
-use std::str::FromStr;
 use rgb::RGB8;
-use textplots::{Chart, ColorPlot, Shape};
+use std::str::FromStr;
+use textplots::{AxisBuilder, Chart, ColorPlot, LabelBuilder, LabelFormat, LineStyle, Shape};
 
 #[derive(Parser)]
 struct Cli {
@@ -20,11 +20,13 @@ struct ReportLine {
 struct ReportLineParsingError;
 
 impl FromStr for ReportLine {
-    type Err = ReportLineParsingError; 
+    type Err = ReportLineParsingError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let split = s.split(" ").collect::<Vec<&str>>();
-        let naive = split[0].parse::<NaiveDateTime>().expect("Could not parse date");
+        let naive = split[0]
+            .parse::<NaiveDateTime>()
+            .expect("Could not parse date");
         let date = naive.and_local_timezone(Local).single().unwrap();
         let open: u8 = split[1].parse::<u8>().expect("Could not open tasks");
         let closed: u8 = split[2].parse::<u8>().expect("Could not open tasks");
@@ -35,8 +37,8 @@ impl FromStr for ReportLine {
 fn to_point(min_date: &DateTime<Local>, date: &DateTime<Local>, count: &u8) -> (f32, f32) {
     let diff = *date - *min_date;
     let minutes = diff.num_seconds() as f32;
-    let x:f32 = minutes;
-    let y:f32 = *count as f32;
+    let x: f32 = minutes;
+    let y: f32 = *count as f32;
     (x, y)
 }
 
@@ -44,20 +46,33 @@ fn get_points_from_report_lines<'a>(lines: Vec<ReportLine>) -> (Vec<(f32, f32)>,
     let min = lines[0].date;
     lines
         .into_iter()
-        .map(|line| { (to_point(&min, &line.date, &(line.closed)), to_point(&min, &line.date, &(line.open + line.closed))) })
+        .map(|line| {
+            (
+                to_point(&min, &line.date, &(line.closed)),
+                to_point(&min, &line.date, &(line.open + line.closed)),
+            )
+        })
         .unzip()
 }
 
 fn main() {
     let args = Cli::parse();
-    let entries = std::fs::read_to_string(args.path).unwrap().lines().map(|l| { ReportLine::from_str(l).unwrap() }).collect::<Vec<ReportLine>>();
+    let entries = std::fs::read_to_string(args.path)
+        .unwrap()
+        .lines()
+        .map(|l| ReportLine::from_str(l).unwrap())
+        .collect::<Vec<ReportLine>>();
     let (completed, open) = get_points_from_report_lines(entries);
     let max: f32 = completed.last().copied().unwrap().0;
     let red = RGB8::new(255u8, 0u8, 0u8);
     let green = RGB8::new(0u8, 255u8, 0u8);
 
-    Chart::new(180,60,-10f32,max + 10f32)
+    Chart::new(180, 60, -10f32, max + 10f32)
         .linecolorplot(&Shape::Lines(&completed), green)
         .linecolorplot(&Shape::Lines(&open), red)
+        .x_axis_style(LineStyle::None)
+        .y_axis_style(LineStyle::None)
+        .x_label_format(LabelFormat::None)
+        .y_label_format(LabelFormat::None)
         .display();
 }
